b42839dc56e7afbde10d06a19463478666fc1c54
Osemudiamhen Ughu <oseughu@gmail.com>
2025-02-04 20:51:27 +0000

    add onsite services calculator

---
+++ lib/tax_calculator/onsite_services_calculator.rb
@@ -0,0 +1,21 @@
+require_relative "base_calculator"
+
+module TaxCalculator
+  class OnsiteServicesCalculator < BaseCalculator
+    def calculate
+      transaction, tax_rate = case
+      when service_location == "Spain"
+        [ "service/onsite", VAT_SPANISH ]
+      when in_eu?(service_location) && buyer_type == :individual
+        [ "service/onsite", local_vat_for(service_location) ]
+      when in_eu?(service_location) && buyer_type == :company
+        [ "reverse charge", 0.0 ]
+      else
+        [ "export", 0.0 ]
+      end
+
+      tax_amount = price * tax_rate
+      { transaction_type: transaction, tax_rate: tax_rate, tax_amount: tax_amount }
+    end
+  end
+end
