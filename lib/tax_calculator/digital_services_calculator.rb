c897e1fd9b7d694669d27e3c8b6e37a45b6aad96
Osemudiamhen Ughu <oseughu@gmail.com>
2025-02-04 21:01:23 +0000

    add digital services calculator

---
+++ lib/tax_calculator/digital_services_calculator.rb
@@ -0,0 +1,21 @@
+require_relative "base_calculator"
+
+module TaxCalculator
+  class DigitalServicesCalculator < BaseCalculator
+    def calculate
+      transaction, tax_rate = case
+      when buyer_country == "Spain"
+        [ "service/digital", VAT_SPANISH ]
+      when in_eu?(buyer_country) && buyer_type == :individual
+        [ "service/digital", local_vat_for(buyer_country) ]
+      when in_eu?(buyer_country) && buyer_type == :company
+        [ "reverse charge", 0.0 ]
+      else
+        [ "service/digital", 0.0 ]
+      end
+
+      tax_amount = price * tax_rate
+      { transaction_type: transaction, tax_rate: tax_rate, tax_amount: tax_amount }
+    end
+  end
+end
